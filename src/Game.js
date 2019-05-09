c1aa3ba927c0e837ef5a271c09f627f58a571afc
Saewon Hwang <saebeezy@Saewons-MacBook-Pro.local>
2019-05-09 16:18:47 -0700

    Removed uncessary files

---
+++ src/Game.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Games.css';
+
+
+const CELL_SIZE = 20;
+const WIDTH = 800;
+const HEIGHT = 600;
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className='Board'
+                style={{
+                    width: WIDTH, height: HEIGHT,
+                    backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`
+                }}>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Game;
