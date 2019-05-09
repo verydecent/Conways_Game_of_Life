84d70dfc275c0cf1c14cf0177dc5bb3ec242c2e4
Saewon Hwang <saebeezy@Saewons-MacBook-Pro.local>
2019-05-09 16:56:18 -0700

    Creates Cell Component

--- src/Game.js
+++ src/Game.js
@@ -6,6 +6,24 @@ const CELL_SIZE = 20;
 const WIDTH = 800;
 const HEIGHT = 600;
 
+class Cell extends React.Component {
+    render() {
+        const { x, y } = this.props;
+        return (
+            <div 
+            className="Cell"
+            style={{
+                left: `${CELL_SIZE * x + 1}px`,
+                top:  `${CELL_SIZE * y + 1}px`,
+                width: `${CELL_SIZE - 1}px`,
+                height: `${CELL_SIZE - 1}px`,
+            }}>
+
+            </div>
+        )
+    }
+}
+
 class Game extends React.Component {
     constructor() {
         super();
